c061bafba354b0d0320a011f51abaa5ee742f1ca
alinmocanu <alin.mocanu95@gmail.com>
2019-03-29 01:49:49 +0200

    Some Elixir exercise

--- lib/cinema_elixir.ex
+++ lib/cinema_elixir.ex
@@ -16,3 +16,27 @@ defmodule CinemaElixir do
     :world
   end
 end
+
+defmodule PlanificareSali do
+  use GenServer
+
+  @spec start_link() :: :ignore | {:error, any()} | {:ok, pid()}
+  def start_link do
+    GenServer.start_link(PlanificareSali,"Hello")
+  end
+
+  @spec init(any()) :: {:ok, any()}
+  def init(initial_data) do
+    greetings = %{:greeting => initial_data}
+    {:ok,greetings}
+  end
+
+  @spec get_my_state(atom() | pid() | {atom(), any()} | {:via, atom(), any()}) :: any()
+  def get_my_state(process_id) do
+    GenServer.call(process_id,{:get_the_state})
+  end
+
+  def handle_call({:get_the_state}, _from, my_state) do
+    {:reply, my_state, my_state}
+  end
+end
