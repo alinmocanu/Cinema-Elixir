f3a89be5410b3465ead632960091835044b59050
alinmocanu <sorin.mocanu95@yahoo.com>
2019-03-28 23:40:50 +0200

    First commit

---
+++ lib/cinema_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CinemaElixir do
+  @moduledoc """
+  Documentation for CinemaElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CinemaElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cinema_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CinemaElixirTest do
+  use ExUnit.Case
+  doctest CinemaElixir
+
+  test "greets the world" do
+    assert CinemaElixir.hello() == :world
+  end
+end
