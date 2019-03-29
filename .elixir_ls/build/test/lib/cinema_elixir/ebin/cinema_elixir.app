233ba5343c7a4c1d9bb9367418740a9bf2496f7c
alinmocanu <alin.mocanu95@gmail.com>
2019-03-30 01:44:19 +0200

    Add files

---
+++ .elixir_ls/build/test/lib/cinema_elixir/ebin/cinema_elixir.app
@@ -0,0 +1,8 @@
+{application,cinema_elixir,
+             [{applications,[kernel,stdlib,elixir]},
+              {description,"cinema_elixir"},
+              {modules,['Elixir.CinemaElixir','Elixir.God',
+                        'Elixir.PlanificareSali','Elixir.Program']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {mod,{'Elixir.CinemaElixir',[]}}]}.
---
+++ lib/Program.ex
@@ -0,0 +1,22 @@
+defmodule Program do
+    use Agent
+
+    def start_link(initial_value) do
+        Agent.start_link(fn -> initial_value end, name: __MODULE__)
+    end
+    
+    def value do
+        Agent.get(__MODULE__, & &1)
+    end
+    
+    def increment do
+        Agent.update(__MODULE__, &(&1 + 1))
+    end
+end
+
+Program.start_link(0)
+
+Program.value     #=> 0
+Program.increment #=> :ok
+Program.increment #=> :ok
+Program.value     #=> 2
