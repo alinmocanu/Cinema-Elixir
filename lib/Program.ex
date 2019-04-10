64fd1c3c760686f7c41a9e6ce4f9b52801d4b9b0
alinmocanu <alin.mocanu95@gmail.com>
2019-04-11 02:12:06 +0300

    Try read file with agent

--- lib/Program.ex
+++ lib/Program.ex
@@ -1,15 +1,16 @@
 defmodule Program do
     use Agent
 
-    def start_link() do
+    def start_link do
         #Agent.start_link(fn -> initial_value end, name: __MODULE__)
         #{:ok,file} = File.open("Program.txt",[:read,:compressed])
-        File.open("Program.txt")
-        Agent.start_link(fn -> File.read("Program.txt") end, name: __MODULE__)
+        File.open("lib/Program.txt")
+        Agent.start_link(fn -> File.read("lib/Program.txt") end, name: __MODULE__)
     end
     
     def value do
-        Agent.get(__MODULE__, & &1)
+        #Agent.get(__MODULE__, & &1)
+        Agent.get(__MODULE__,fn(state) -> Map.get(state,1)end)
     end
     
     # def increment do
@@ -18,7 +19,20 @@ defmodule Program do
 
     def write do
         #base = File.read("Program.txt")
-        Agent.update(__MODULE__,&(File.copy("Program.txt",&1)))
+        #Agent.update(__MODULE__,&(File.copy("lib/Program.txt",&1)))
+        Agent.update(__MODULE__,fn(state) -> Map.put(state,1,Program.read) end)
+    end
+
+    #imi afiseaza la consola iex ce are fisierul
+    def read do
+        {:ok,contents} = File.read("lib/Program.txt")
+        contents
+    end
+
+    def do_stuff do
+        {:ok,contents} = Program.start_link
+        Program.write
+        Program.value
     end
 end
 
