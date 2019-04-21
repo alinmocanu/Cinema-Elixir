09cb4790a6df30c3307d9a3557c841ba8df36629
Cosmin Alex <silvian1995@yahoo.com>
2019-04-21 17:08:18 +0300

    Add Agent for file read

--- lib/cinemaelixir_tweet/scheduler.ex
+++ lib/cinemaelixir_tweet/scheduler.ex
@@ -3,7 +3,7 @@ defmodule CinemaElixir.Scheduler do
      otp_app: :cinema_elixir
     def schedule_file() do
  
-      hashtag = CinemaElixir.FileReader.get_hashtag("C:\\Users\\Cosmin Alex\\Desktop\\Cinema-Elixir\\lib\\cinemaelixir_tweet\\movie_quotes.txt")
+      hashtag = CinemaElixir.FileReader.start("C:\\Users\\Cosmin Alex\\Desktop\\Cinema-Elixir\\lib\\cinemaelixir_tweet\\movie_quotes.txt")
         CinemaElixir.ReadTweets.get_tweets(hashtag)
         |> CinemaElixir.TweetServer.tweet
     end
