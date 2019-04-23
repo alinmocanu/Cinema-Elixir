ff991b37f484e74d439ed1d2de724e0c2ba7d8c4
Cosmin Alex <silvian1995@yahoo.com>
2019-04-23 11:35:05 +0300

    resolve error

--- lib/adaug_filme.ex
+++ lib/adaug_filme.ex
@@ -43,10 +43,10 @@ defmodule AdaugFilme do
         File.write("lib/Program.txt", movieHour, [:append]) |> IO.inspect
         File.write("lib/Program.txt", movieHall, [:append]) |> IO.inspect
         File.write("lib/Program.txt", movieAgeLimit, [:append]) |> IO.inspect
-        File.write("lib/cinemaelixir_tweet/movie_quotes.txt","\n\n"<> movieHashtag, [:append]) |> IO.inspect
+        File.write("lib/cinemaelixir_tweet/movie_quotes.txt", movieHashtag, [:append]) |> IO.inspect
 
         CinemaElixir.FileReader.update(movieHashtag)
-        MovieDatabase.add_movie_to_database(movieTitle, movieHour, movieDate, movieHall, movieAgeLimit
+        MovieDatabase.add_movie_to_database(movieTitle, movieHour, movieDate, movieHall, movieAgeLimit)
 
     
       end
